113086cb1c221c47b748b5fefe353706cb4ed1b7
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2023-03-01 19:08:15 -0800

    Import Nav Component
    - Import Drawer from MUI Library
    - Refactor Drawer text and icons for dummy sidebar items

--- weather-dashboard/src/App.js
+++ weather-dashboard/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
     </div>
   );
 }
